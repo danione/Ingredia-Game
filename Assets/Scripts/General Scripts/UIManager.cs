b87771077e0a588cf3f79cccc10b29912148d2bf
danione <yordan.yankulov@icloud.com>
2023-11-27 23:28:23 +0000

    health upgrades initilised
    - can now add health upgrades (tested and it worked)

--- Assets/Scripts/General Scripts/UIManager.cs
+++ Assets/Scripts/General Scripts/UIManager.cs
@@ -1,11 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class UIManager : MonoBehaviour
 {
     public static UIManager Instance;
+
     [SerializeField] private Transform inventoryMenu;
+    [SerializeField] private Transform healthUIItem;
+    [SerializeField] private List<HealthUpgrade> healthUpgrades = new();
 
     private void Start()
     {
@@ -25,10 +29,47 @@ public class UIManager : MonoBehaviour
     private void OnUpgradesMenuOpened()
     {
         inventoryMenu.gameObject.SetActive(true);
+        UpdateScreen();
     }
 
     public void OnUpgradesMenuClosed()
     {
         inventoryMenu.gameObject.SetActive(false);
+    }
+
+    private void UpdateScreen()
+    {
+        HealthUpgrade();
+    }
+
+    private void HealthUpgrade()
+    {
+        string upgradeName;
+        string cost;
+        if (healthUpgrades.Count > 0)
+        {
+            upgradeName = healthUpgrades[0].upgradeName;
+            cost = healthUpgrades[0].cost.ToString();
+        }
+        else
+        {
+            upgradeName = "Latest Endurance Upgrade Received";
+            cost = "";
+            healthUIItem.GetChild(2).gameObject.SetActive(false);
+        }
+
+        healthUIItem.GetChild(0).GetComponent<TextMeshProUGUI>().text = upgradeName;
+        healthUIItem.GetChild(1).GetComponent<TextMeshProUGUI>().text = cost;
+    }
+
+    public void BuyHealthUpgrade()
+    {
+        if(healthUpgrades.Count > 0)
+        {
+            healthUpgrades[0].ApplyUpgrade(PlayerController.Instance.gameObject);
+            healthUpgrades.RemoveAt(0);
+            UpdateScreen();
+        }
+        UpdateScreen();
     }
 }
