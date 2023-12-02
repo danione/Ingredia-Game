fb4471a19fa19f6290173eca78b09c2d5f47c71d
danione <yordan.yankulov@icloud.com>
2023-12-02 19:04:08 +0000

    added scroll slips upgrade
    - added the scriptable objects
    - will need to complete the upgrades first to implement the spellbook

---
+++ Assets/Scripts/Scriptable Objects/Scriptable Objects Base/Upgrades/ScrollSlipUpgrade.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu(fileName = "New Scroll Slip", menuName = "Scriptable Objects/Upgrades/Scroll Slip")]
+public class ScrollSlipUpgrade : UpgradeData
+{
+    public RitualScriptableObject ritual;
+
+    public override void ApplyUpgrade(GameObject obj)
+    {
+        // 
+    }
+}
