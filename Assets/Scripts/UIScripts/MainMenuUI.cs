07e34e8eb694348ea613cc4feafd0238eb920c98
danione <yordan.yankulov@icloud.com>
2024-01-28 17:14:03 +0000

    main menu finished
    - integrated a new scene with the main menu

---
+++ Assets/Scripts/UIScripts/MainMenuUI.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuUI : MonoBehaviour
+{
+    public void GoToTutorialStage()
+    {
+        SceneManager.LoadScene("Tutorial Level");
+    }
+
+    public void GoToNormalStage()
+    {
+        SceneManager.LoadScene("Normal Level");
+    }
+
+    public void ExitGame()
+    {
+        #if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+        #endif
+        Application.Quit();
+    }
+}
