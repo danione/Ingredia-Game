edbcaf76cb4040dee7b86ed78dde7a13b3ada9ea
danione <yordan.yankulov@icloud.com>
2024-01-30 11:43:08 +0000

    status changes
    - timer changes based on status
    - a few bugs are present

--- Assets/Scripts/UIScripts/TimerUIManager.cs
+++ Assets/Scripts/UIScripts/TimerUIManager.cs
@@ -14,12 +14,12 @@ public class TimerUIManager : MonoBehaviour
 
     [SerializeField] private TimerDataObject testObject;
     [SerializeField] private PotionsData testPotion;
+    private bool currentGhostState = false;
 
     void Start()
     {
         activeTimers = new List<Transform>();
         GameEventHandler.Instance.GhostActivated += OnGhostActivated;
-
     }
 
     private void Update()
@@ -34,7 +34,32 @@ public class TimerUIManager : MonoBehaviour
     private void OnGhostActivated()
     {
         activeTimers.Add(CreateANewTimer());
+        PlayerEventHandler.Instance.TransformIntoGhost += OnTransformIntoGhost;
     }
+
+    private void OnTransformIntoGhost(bool isTransforming)
+    {
+        if (currentGhostState != isTransforming) 
+        {
+            ChangeImages(isTransforming);
+            currentGhostState = isTransforming;  
+        }
+    }
+
+    private void ChangeImages(bool isTransforming)
+    {
+        if (isTransforming)
+        {
+            activeTimers[0].GetChild(0).GetComponent<Image>().sprite = testObject.BorderSpriteEngaged;
+            activeTimers[0].GetChild(1).GetComponent<Image>().sprite = testObject.IconSpriteEngaged;
+        }
+        else
+        {
+            activeTimers[0].GetChild(0).GetComponent<Image>().sprite = testObject.BorderSpriteIdle;
+            activeTimers[0].GetChild(1).GetComponent<Image>().sprite = testObject.IconSpriteIdle;
+        }
+    }
+
 
     private Transform CreateANewTimer()
     {
