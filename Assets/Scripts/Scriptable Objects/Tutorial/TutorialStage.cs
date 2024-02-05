d0fded17175e26e029be3fb9b002ab7703c7a22e
danione <yordan.yankulov@icloud.com>
2024-02-05 09:16:26 +0000

    tutorial descriptions
    - modified the descriptions for clarity

--- Assets/Scripts/Scriptable Objects/Tutorial/TutorialStage.cs
+++ Assets/Scripts/Scriptable Objects/Tutorial/TutorialStage.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 public abstract class TutorialStage : ScriptableObject
 {
     public string title;
+    [TextArea(4, 10)] 
     public string description;
     public abstract void InitiateStage();
     public abstract void NextStage();
