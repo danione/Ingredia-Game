d484ccf043e2d4e8126ee68cd4b55035850f2b73
danione <yordan.yankulov@icloud.com>
2024-03-11 13:51:52 +0000

    grimoire screen
    - removed the scroll slip screen
    - adding a grimoire screen

--- Assets/Scripts/UIScripts/ScrollSlipUIManager.cs
+++ Assets/Scripts/UIScripts/ScrollSlipUIManager.cs
@@ -11,15 +11,12 @@ public class ScrollSlipUIManager : MonoBehaviour
     [SerializeField] private Transform countScrollsUI;
     [SerializeField] private Transform normalSlipsContainerUI;
 
-    private int latestActivatedNumber = 0;
-    private int slipCellsPerRow;
 
     private void Start()
     {
         GameEventHandler.Instance.ScrollSlipGenerated += OnScrollSlipGenerated;
         PlayerEventHandler.Instance.OpenedScrollsMenu += OnScrollSlipMenuOpen;
         PlayerEventHandler.Instance.ClosedAllOpenMenus += OnCloseMenu;
-        slipCellsPerRow = normalSlipsContainerUI.GetChild(0).childCount;
     }
 
     private void OnDestroy()
@@ -56,69 +53,9 @@ public class ScrollSlipUIManager : MonoBehaviour
 
         scrollSlipMenu.gameObject.SetActive(!scrollSlipMenu.gameObject.activeSelf);
 
-        PopulateMenus();
+        //PopulateMenus();
     }
 
-
-    private void PopulateMenus()
-    {
-        KeyValuePair<int, int> scrollSlipsCount = GameManager.Instance.SlipManager.GetScrollSlipsCount();
-
-        countScrollsUI.GetChild(0).GetComponent<TextMeshProUGUI>().text = scrollSlipsCount.Key.ToString(); // current
-        countScrollsUI.GetChild(2).GetComponent<TextMeshProUGUI>().text = scrollSlipsCount.Value.ToString(); // available
-
-        PopulateNormalSlips(scrollSlipsCount.Key);
-    }
-
-    private void PopulateNormalSlips(int currentNumberOfSlips)
-    { 
-        int neededRows = (currentNumberOfSlips + slipCellsPerRow - 1) / slipCellsPerRow; // round up to the nearest value
-
-        if(neededRows + 1 > normalSlipsContainerUI.childCount)
-        {
-            CreateNewRows(neededRows + 1 - normalSlipsContainerUI.childCount);
-        }
-
-        ActivateNormalUI(currentNumberOfSlips);
-    }
-
-    private void CreateNewRows(int number)
-    {
-        for (int i = 0; i < number; i++)
-        {
-            GameObject duplicatedRow = Instantiate(normalSlipsContainerUI.GetChild(0).gameObject);
-
-            // Set the parent transform for the duplicated UI element
-            duplicatedRow.transform.SetParent(normalSlipsContainerUI);
-
-            duplicatedRow.SetActive(true);
-        }
-    }
-
-    private void ActivateNormalUI(int currentNumberOfSlips)
-    {
-        if (currentNumberOfSlips == latestActivatedNumber) return;
-    
-        while(latestActivatedNumber < currentNumberOfSlips)
-        {
-            int row = (latestActivatedNumber / slipCellsPerRow) + 1; // ingore the first row
-            // Get the row and next activatable cell
-            PopulateCellObject(normalSlipsContainerUI.GetChild(row)?.GetChild(latestActivatedNumber % slipCellsPerRow), latestActivatedNumber);
-            latestActivatedNumber++;
-        }
-    }
-
-    private void PopulateCellObject(Transform slipObject, int index)
-    {
-        // fetch ritual info and populate the cell
-        RitualScriptableObject data = GameManager.Instance.SlipManager.GetSlipByIndex(index);
-        if (slipObject == null || data == null) return;
-
-        slipObject.gameObject.SetActive(true);
-        slipObject.GetChild(0).GetComponent<TextMeshProUGUI>().text = data.ritualName; // name of ritual
-        slipObject.GetChild(2).GetComponent<TextMeshProUGUI>().text = data.description; // name of ritual
-        slipObject.GetChild(5).GetComponent<TextMeshProUGUI>().text = ScrollPopup.RitualIngredientsToString(data); // ingredients of ritual
-    }
 }
 
 [System.Serializable]
